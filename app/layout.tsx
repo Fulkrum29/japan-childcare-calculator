db14e47c0e7c9b88dc1a4880130801ff309ef15f
Your Name <youremail@example.com>
2026-05-06 11:50:31 +0900

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -46,7 +47,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+
+  {children}
+
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-R8WCPGZGCQ"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-R8WCPGZGCQ');
+    `}
+  </Script>
+
+</body>
     </html>
   );
 }
