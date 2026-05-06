3fd267b23f503832f403f96b1ad72f654d837c9a
Your Name <youremail@example.com>
2026-05-06 12:03:08 +0900

    Move analytics script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,24 +47,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+
+  <head>
+
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-R8WCPGZGCQ"
+      strategy="afterInteractive"
+    />
+
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-R8WCPGZGCQ');
+      `}
+    </Script>
+
+  </head>
+
+  <body>
 
   {children}
-
-  <Script
-    src="https://www.googletagmanager.com/gtag/js?id=G-R8WCPGZGCQ"
-    strategy="afterInteractive"
-  />
-
-  <Script id="google-analytics" strategy="afterInteractive">
-    {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-R8WCPGZGCQ');
-    `}
-  </Script>
 
 </body>
     </html>
